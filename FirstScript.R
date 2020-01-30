cb02b4fdc4d1b1190896dd785fe8e249065cf6df
Aura M. Alonso-Rodríguez <aalonsor@uvm.edu>
2020-01-30 14:32:14 -0500

    uploading practice script and markdown from class

---
+++ FirstScript.R
@@ -0,0 +1,20 @@
+# Our first R script!
+# Let's develop good habits and start annotating so we know what this file is.
+# It feels redundant for now, but it is necessary.
+#
+# January 30 2020
+# AAR
+
+# Preliminaries -----------------------------------------------------------
+
+#install.packages("ggplot2")
+library(ggplot2)
+
+# -------------------------------------------------------------------------
+runif(10) #generate 10 random uniforms
+getwd() #get location of working directory
+
+# R functions always have parenthesis associated with them.
+
+
+
---
+++ MarkdownPractice_20200130.R
@@ -0,0 +1,21 @@
+## ----setup, include=FALSE----------------------------------------------
+knitr::opts_chunk$set(echo = TRUE)
+
+
+## ----echo=TRUE, eval=FALSE---------------------------------------------
+## # We want to get in the habit of commenting all of our code, because that is the only way to make our work repeatable. Make comments!
+## runif(10)
+## 
+## # Using eval=FALSE will remove the output from the html file. Using echo=FALSE will remove the chunk code from the html file.
+
+
+## pwd
+
+
+## ----cars--------------------------------------------------------------
+summary(cars)
+
+
+## ----pressure, echo=FALSE----------------------------------------------
+plot(pressure)
+
